d701ef0181834bbae074f6c982a8117df9ddf587
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-06-05 00:09:18 +0300

    it will add the image model

--- insta_clone/models.py
+++ insta_clone/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Image(models.Model):
+    image = CloudinaryField('image')
+    name = models.CharField(max_length=144, blank=True, default="Post")
+    caption = models.TextField(blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField(default=0)
+    profile = models.ForeignKey(User, on_delete=models.CASCADE)
+    # user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+   
--- instagram/settings.py
+++ instagram/settings.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'insta_clone',
     'bootstrap4',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +130,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+cloudinary.config( 
+  cloud_name = "", 
+  api_key = "", 
+  api_secret = "" 
+)
