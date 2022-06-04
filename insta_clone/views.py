9b993eb7984c44f21f67b853155765b01b6d7153
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-06-04 16:13:28 +0300

    it will add the login.html

--- insta_clone/views.py
+++ insta_clone/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required.
+# from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
-@login_required(login_url='/accounts/login/')
-def article(request, article_id):
-    ....
+
 
 def index(request):
     title='welcome'
     
     return render(request,'insta/index.html',{'title':title})
+
+# @login_required(login_url='/accounts/login/')
+# def article(request, article_id):
+    
