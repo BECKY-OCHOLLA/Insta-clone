c4a4a604add89e4a0aa4a897eb013b688e68f544
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-06-05 00:35:45 +0300

    it will add the comment model and delete functions on image model

--- insta_clone/models.py
+++ insta_clone/models.py
@@ -12,4 +12,34 @@ class Image(models.Model):
     likes = models.IntegerField(default=0)
     profile = models.ForeignKey(User, on_delete=models.CASCADE)
     # user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+
+class UserProfile(models.Model):
+    # User=models.ForeignKey(User, on_delete=models.CASCADE)
+    profile_Photo=CloudinaryField('image')
+    bio=models.TextField(blank=True)
+    followers = models.ManyToManyField(User, related_name="followers", blank=True)
+    following = models.ManyToManyField(User, related_name="following", blank=True)
+
+    def __str__(self):
+        return self.bio
+
+    def save_post(self):
+        self.save()
+
+    def delete_post(self):
+        self.delete()
+
+    def update_caption(self, new_cap):
+        self.caption = new_cap
+        self.save()    
+
+class Comment(models.Model):
+    comment = models.CharField(max_length=256)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Image, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.comment
+
+
    
