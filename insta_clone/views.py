f2fcbf98a2cf7873d445d6ed3991e1ccbc1c3320
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-06-04 17:02:23 +0300

    it will import login and logut

--- insta_clone/views.py
+++ insta_clone/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
-# from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
 
-
+@login_required(login_url='/accounts/login/')
 def index(request):
     title='welcome'
     
     return render(request,'insta/index.html',{'title':title})
 
-# @login_required(login_url='/accounts/login/')
-# def article(request, article_id):
-    
+def user_login(request):
+    if request.method=="POST":
+        username=request.POST.get("username")
+        password=request.POST.get("password")
+        user = authenticate(username=username, password=password)
