56c7d2745937c2efbca3334eb8425518abea4eb4
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-06-05 13:37:57 +0300

    it will add posts and like view

--- insta_clone/views.py
+++ insta_clone/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from .models import Image, User, UserProfile, Comment
-# from django.contrib.auth import authenticate, login, logout
-# from django.http import HttpResponseRedirect
-# from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 # Create your views here.
@@ -41,26 +41,64 @@ def profile(request, id):
     return render(request, "insta/profile.html", context={"user":user,"profile":profile,"posts":posts})
 
 
-# @login_required(login_url='/accounts/login/')
-# def user_login(request):
-#     if request.method=="POST":
-#         username=request.POST.get("username")
-#         password=request.POST.get("password")
-#         user = authenticate(username=username, password=password)
+@login_required
+def user_login(request):
+    if request.method=="POST":
+        username=request.POST.get("username")
+        password=request.POST.get("password")
+        user = authenticate(username=username, password=password)
 
-#         if user:
-#             if user is not None:
-#               login(request, user)
-#               return HttpResponseRedirect(reverse("index"))
+        if user:
+            if user is not None:
+              login(request, user)
+              return HttpResponseRedirect(reverse("index"))
              
-#             else:
-#                 return HttpResponseRedirect(reverse("user_login")) #raise error/ flash
-#     else:
-#         return render(request, "auth/login.html", context={})
+            else:
+                return HttpResponseRedirect(reverse("user_login")) #raise error/ flash
+    else:
+        return render(request, "registration/login.html", context={})
 
-# @login_required(login_url='/accounts/login/')
-# def user_logout(request):
-#     logout(request)
-#     return HttpResponseRedirect(reverse("user_login"))
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("user_login"))
+
+def post(request, id):
+    post = Image.objects.get(id = id)
+    comments = Comment.objects.filter(post__id=id)
+    current_user = request.user
+    current_profile = UserProfile.objects.get(post=id)
+
+    if request.method == "POST":
+        comment_form = CommentForm(request.POST)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.user = current_user
+            comment.post = post
+            comment.save()
+            comment_form = CommentForm()
+            return redirect("post", post.id)
+
+    else:
+        comment_form = CommentForm()
+
+    return render(request, "insta/post.html", context={"post":post,"current_user":current_user,"current_profile":current_profile,"comment_form":comment_form,
+                                                          "comments":comments,})
+
+
+def like(request, id):
+    post = Image.objects.get(id = id)
+    post.likes += 1
+    post.save()
+    return HttpResponseRedirect(reverse("index"))
+
+
+def like_post(request, id):
+    post = Image.objects.get(id = id)
+    post.likes += 1
+    post.save()
+    return redirect("post", post.id)
+
 
   
