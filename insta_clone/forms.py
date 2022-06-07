99f3a48bb3b991cc4ea56c36723ab342463bb578
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-06-07 12:41:31 +0300

    updates user views

--- insta_clone/forms.py
+++ insta_clone/forms.py
@@ -24,3 +24,24 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model= Comment
         fields=['comment']
+
+class RegistrationForm(UserCreationForm):
+    username = forms.CharField(max_length=50)
+    email=forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ('username','email','password1','password2')
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    bio = forms.CharField() 
+
+    class Meta:
+        model = User
+        fields = ['username','email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields =['image']
--- insta_clone/views.py
+++ insta_clone/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.decorators import login_required
 from .models import Image,Profile, Comment
 from django.contrib.auth.models import User
 from django.http import HttpResponse
-from .forms import ProfileForm,ImageForm,CommentForm
+from django.shortcuts import render,redirect,HttpResponse
+from django.contrib import messages
+from .forms import *
+from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth.decorators import login_required
+from .models import *
+
 
 
 # Create your views here.
@@ -85,4 +90,42 @@ def post(request, id):
     return render(request, "insta/post.html", context={"post":post,"current_user":current_user,"current_profile":current_profile,"comment_form":comment_form,
                                                           "comments":comments,})
 
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            
+            form.save()
+
+            username = form.cleaned_data.get('username')
+            messages.success(request, f' Account for {username} has been created successfully!')
+            return redirect('home')
+    else:
+        form = RegistrationForm()
+    return render(request, 'django_registration/registration_form.html', {'form':form})
+
+def profile(request):
+    if request.method == 'POST':
+
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        prof_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user)
+
+        if  prof_form.is_valid():
+            user_form.save()
+            prof_form.save()
+
+            return redirect('profile')
+
+    else:
+        
+        prof_form = ProfileUpdateForm(instance=request.user)
+        user_form = UserUpdateForm(instance=request.user)
+
+        context = {
+            'user_form':user_form,
+            'prof_form': prof_form
+        }
+
+        return render(request, 'profile/profile.html', context)
 
