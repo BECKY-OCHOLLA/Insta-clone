e58639ff2223cc26c4a8ef27e805f65fd4186f7e
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-06-12 12:39:48 +0300

    add secret key

--- insta_becky/settings.py
+++ insta_becky/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '23gdwfji#=24-=*0j9n%%j-zjd&h*(5^ireu44^i9vgiuw^+du'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
